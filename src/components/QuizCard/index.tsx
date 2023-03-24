f6eee50064cc2843d9f998ad1a85802ad737b833
nykael <nykaelccardial@gmail.com>
2023-03-24 17:37:23 -0300

    FLatList Animated

--- src/components/QuizCard/index.tsx
+++ src/components/QuizCard/index.tsx
@@ -1,5 +1,10 @@
 import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
 
+import Animated, {
+  FadeInUp
+
+} from 'react-native-reanimated';
+
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
 
@@ -8,13 +13,17 @@ import { QUIZZES } from '../../data/quizzes';
 
 type Props = TouchableOpacityProps & {
   data: typeof QUIZZES[0];
+  index: number;
 }
 
-export function QuizCard({ data, ...rest }: Props) {
+const TouchableOpacityAnimated = Animated.createAnimatedComponent(TouchableOpacity)
+
+export function QuizCard({ data, index, ...rest }: Props) {
   const Icon = data.svg;
 
   return (
-    <TouchableOpacity
+    <TouchableOpacityAnimated
+      entering={FadeInUp.delay(index * 100)}
       style={styles.container}
       {...rest}
     >
@@ -29,6 +38,6 @@ export function QuizCard({ data, ...rest }: Props) {
       <Text style={styles.title}>
         {data.title}
       </Text>
-    </TouchableOpacity>
+    </TouchableOpacityAnimated>
   );
 }
